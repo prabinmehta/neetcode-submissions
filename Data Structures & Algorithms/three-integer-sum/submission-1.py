a80424a2338f7248c8501cfc24f64c99bf8986ed
prabinmehta <prabinmehta@gmail.com>
2026-05-10 21:11:49 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        for i,j in enumerate(nums):
+            s = set()
+            # print("s is outisde",s)
+            for k in nums[i+1:]:
+                # print("j is and k is ",j,k)
+                req = -(j+k)
+                # print("req is ",req)
+                if req in s:
+                    # res.add(tuple(sorted(j,req,k)))
+                    res.add(tuple(sorted((j, req, k)))) 
+                s.add(k)
+                # print("s inside else is ", s)
+        return [list(i) for i in res]
+        
